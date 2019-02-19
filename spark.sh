5035fa79976a0cda940cfb1c20362562fb5b7a00
Jordi Nebot <hola@jordinebot.cat>
2019-02-19 05:13:04 +0100

    Include ZIM installer to setup script

--- spark.sh
+++ spark.sh
@@ -1,1 +1,17 @@
 #!/usr/bin/env zsh
+
+# Install ZIM
+if [ ! -d ${ZDOTDIR:-${HOME}}/.zim ]
+then
+    git clone --recursive https://github.com/Eriner/zim.git ${ZDOTDIR:-${HOME}}/.zim
+    
+    setopt EXTENDED_GLOB
+    for template_file ( ${ZDOTDIR:-${HOME}}/.zim/templates/* ); do
+      user_file="${ZDOTDIR:-${HOME}}/.${template_file:t}"
+      touch ${user_file}
+      ( print -rn "$(<${template_file})$(<${user_file})" >! ${user_file} ) 2>/dev/null
+    done
+
+    source ${ZDOTDIR:-${HOME}}/.zlogin
+    chsh -s $(which zsh)
+fi
