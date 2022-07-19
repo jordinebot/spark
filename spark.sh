aa2860a0813c4d86131030f15091e1506d279a4e
Jordi Nebot <hola@jordinebot.cat>
2022-07-19 11:46:16 +0200

    Update spark.sh

--- spark.sh
+++ spark.sh
@@ -8,6 +8,9 @@ defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
 # Install Oh-my-ZSH
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+# Install tmux
+brew install tmux
+
 # Link shell config files
 ln -sf ~/.spark/zshrc ~/.zshrc
 
@@ -24,27 +27,11 @@ ln -sf ~/.spark/tmux.conf ~/.tmux.conf
 brew list --versions neovim || (\
     brew install neovim && \
     mkdir -p ~/.config  && \
-    ln -sf ~/.spark/nvim ~/.config/nvim && \
-    ln -s ~/.spark/coc ~/.config/coc)
+    ln -sf ~/.spark/nvim ~/.config/nvim)
 
-# Install NeoVim plugins requirements
-
-## Ag
-brew list --versions the_silver_searcher || \
-    brew install the_silver_searcher
-
-## Fuzzy Search
-brew list --versions fzy || \
-    brew install fzy
-
-# Install the Fuck
-brew list --versions thefuck || \
-    brew install thefuck
-
-# Install NVM (to handle multiple Node.js installations)
-brew list --versions nvm || \
-    brew install nvm
-
+# Install FNM (to handle multiple Node.js installations)
+brew install nvm
 mkdir -p ~/.nvm
 
+# Create folder for projects
 mkdir -p ~/Code
